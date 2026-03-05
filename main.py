bf94017830f7360073342fa6d6531596329fc997
vamshi <vamshi.y@bitcodingsolutions.com>
2026-03-05 18:50:31 +0530

    first commit

--- main.py
+++ main.py
@@ -12,14 +12,26 @@ import json
 
 weather = OpenWeatherMapAPIWrapper(openweathermap_api_key=os.getenv('OPENWEATHERMAP_API_KEY'))
 
+from pathlib import Path
+Note_FILE='jsonfiles/note.json'
+file_path = Path(Note_FILE)
+if file_path.stat().st_size == 0:
+    with open(Note_FILE,'w') as f:
+        json.dump({'Notes':[]},f)
+        
+Task_File='jsonfiles/task.json' 
+file_path = Path(Task_File)    
+if file_path.stat().st_size==0:
+    with open(Task_File,'w') as f:
+        json.dump({},f)
+
 @tool
 def weather_tool(query:str) -> str:
     """Your are a Weather Tool and get the WEATHER INFO
     Args:
         query (str): City Name
-
     Returns:
-        str: Structure string format only give Current Temperature 
+        str: string
     """
     weather_data = weather.run(query)
     
@@ -37,35 +49,75 @@ def text_summary(query:str) -> str:
     return query
 
 @tool
-def note_tool(query:str) -> str:
-    """
-    Your are the Note making tool 
+def save_note(query:str) -> str:
     
+    """
+    Your are the Note making tool
     Args :
         query(str) : input string
     
     Returns :
         str : string format
     """
-    try:
-        with open("jsonfiles/note.json", "r") as f:
-            data = json.load(f)
-        return data
-    except:
-        with open('jsonfiles/note.json', 'a') as outfile:
-            outfile.write(json.dumps(query))
-            outfile.write(",")
-            outfile.close()
-            return 'Note saved'
+    
+    with open(Note_FILE,'r') as file:
+        json_file=json.load(file)
 
+    for key,value in json_file.items():
+        value=value.append(query)
+
+    with open(Note_FILE,'w') as file:
+        json.dump(json_file,file)
+    return 'Note is Saved'
+   
+@tool
+def Retrieve_note(query:str) -> str:
+    """
+      Your are the Retriever tool to get the saves info
+    """
+            
+    with open(Note_FILE,'r') as file:
+        json_file=json.load(file)
+
+    for key,value in json_file.items():
+        if len(value)==0:
+            return 'Your Note is Empty'
+        else:
+            with open(Note_FILE,'r') as f:
+                data=json.load(f)
+            return data
+@tool
+def add_task(query:str) -> str:
+    """
+    Your are the task manager tool
+    """
+    index=1
+    with open(Task_File,'r') as file:
+        json_file=json.load(file)    
+    data={}
+    for key,value in json_file.items():
+        key={}
+        data[index]=query
+        value=data
+    index+=1  
+    with open(Task_File,'w') as file:
+        json.dump(json_file,file)
+    return 'Task was added'
 
 my_prompt="""  
      Your are the helpFull AI AGENT.
-     1. IF THE GIVEN QUERY RELATED TO THE WEATHER THAN CALL weather_tool THAN EXECUTE WEATHER IN THE CITY ACCORDINGLY.
-     2. IF THE GIVEN QUERY IS RELATED TO THE PARAGRAPH THAN CALL text_summary TOOL AND GIVE SUMMARY OF THE PARAGRAPH.
-     3. IF USER WANTED TO get the information of notes details than get it and BREAK ELSE SAVE THE NOTE.
+     1. IF THE GIVEN QUERY RELATED TO THE WEATHER THAN CALL weather_tool THAN EXECUTE WEATHER IN THE CITY ACCORDINGLY DONT HALUCINATE ANSWERING QUESTION.
+     2. IF THE GIVEN QUERY IS PARAGRAPH ASKING SUMMARY THAN CALL text_summary TOOL AND GIVE SUMMARY OF THE PARAGRAPH DONT HALUCINATE ANSWERING QUESTIONS.
+     3. IF USER WANTED TO add the details in json file than add it.BUT BELOW ARE THE RULES SHOULD MUST FOLLOW.
+        Only add the important text in the user question dont add un-nessesary details to file   
+           Ex: 
+           user: remember I have meeting at 2 pm save the note
+           Give the information only "meeting at 2 pm" 
+           RESULT : NOTE IS SAVED 
+     4. IF USER ASKING ABOUT INFO ABOUT NOTE OR SHOW MY NOTE THEN CALL RETRIEVE_NOTE tool.If data is AVAILABLE THAN SHOW IF empty THEN "your Note is empty"
+     5. If THE USER WANTED TO ADD THE TASK TO THE JSON FILE THAN CALL add_task tool and ADD It to json file as dict object RESPONDE TO THE USER "Task added".
+           
     """
-
 prompt = ChatPromptTemplate.from_messages(
         [
             ("system", my_prompt),
@@ -74,12 +126,11 @@ prompt = ChatPromptTemplate.from_messages(
             MessagesPlaceholder("agent_scratchpad"),
         ]
     )
-
-tools=[weather_tool,text_summary,note_tool]
+tools=[weather_tool,text_summary,save_note,Retrieve_note,add_task]
 
 llm=ChatGroq(
         groq_api_key=os.environ['GROQ_API_KEY'],
-        model="llama-3.3-70b-versatile",
+        model="moonshotai/kimi-k2-instruct-0905",
         )
 
 agent=create_openai_tools_agent(llm,tools,prompt)
@@ -87,7 +138,7 @@ agent=create_openai_tools_agent(llm,tools,prompt)
 agent_executor=AgentExecutor(agent=agent,tools=tools,verbose=True)
 
 result=agent_executor.invoke({"query":
-    """save the note : schedule the meeting at 8 pm """
+    """ What was the weather in berlin? """
 })
 
-print(result['output'])
+print("result:- ", result['output'])
